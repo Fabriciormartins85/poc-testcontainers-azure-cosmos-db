19db796f57ec5cbfc1b798b8a069acdd83aa1b33
Fabricio Martins <forh@gft.com>
2023-02-28 07:09:52 -0300

    Added adjust to create and starting emulator

--- src/test/java/br/com/poc/example/poctestcosmos/infra/AzureCosmosDbConfig.java
+++ src/test/java/br/com/poc/example/poctestcosmos/infra/AzureCosmosDbConfig.java
@@ -3,12 +3,10 @@ package br.com.poc.example.poctestcosmos.infra;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.security.KeyStore;
-import java.util.function.Supplier;
+import java.time.Duration;
 
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.testcontainers.containers.CosmosDBEmulatorContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -16,52 +14,38 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Testcontainers
 @NoArgsConstructor
-class AzureCosmosDbConfig implements  ApplicationContextInitializer<ConfigurableApplicationContext>{
+class AzureCosmosDbConfig implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
     @Container
     private static CosmosDBEmulatorContainer emulator = new CosmosDBEmulatorContainer(
-            DockerImageName.parse("mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest"));
-
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        try {
-            Supplier<Object> emulatorEndpoint = () -> String.valueOf(emulator.getEmulatorEndpoint());
-            Supplier<Object> semulatorKey = () -> String.valueOf(emulator.getEmulatorKey());
-            System.out.println("Container");
-            System.out.println(emulatorEndpoint.get());
-            System.out.println(semulatorKey.get());
-            registry.add("spring.cloud.azure.cosmos.endpoint", emulatorEndpoint);
-            registry.add("spring.cloud.azure.cosmos.key", semulatorKey);
-            System.out.println("Criando emulador");
-    
-            System.out.println("Criado emulador");
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+            DockerImageName.parse("mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest"))
+            .withStartupTimeout(Duration.ofMinutes(3)).withEnv("AZURE_COSMOS_EMULATOR_PARTITION_COUNT", "4");
 
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
-        if(emulator.isCreated() && emulator.isRunning()){
-            return;
-        }
-        emulator.start();
-        var emulatorEndpoint = emulator.getEmulatorEndpoint();
-        var emulatorKey = emulator.getEmulatorKey();
-        try{
-        createKeyStoreFromAzureCosmosAccess();
-        TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
-            applicationContext, "spring.cloud.azure.cosmos.endpoint="+ emulatorEndpoint, 
-            "spring.cloud.azure.cosmos.key="+emulatorKey);
+        try {
+            log.info("\n\n Try to start container");
+            emulator.start();
+            log.info("\n\n Container is started");
+            log.info("Try to bind properties");
+            var emulatorEndpoint = emulator.getEmulatorEndpoint();
+            var emulatorKey = emulator.getEmulatorKey();
+            createKeyStoreFromAzureCosmosAccess();
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+                    applicationContext, "spring.cloud.azure.cosmos.endpoint=" + emulatorEndpoint,
+                    "spring.cloud.azure.cosmos.key=" + emulatorKey);
+            log.info("Finally bind properties");
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
     }
 
-    public void createKeyStoreFromAzureCosmosAccess() throws Exception{
+    private static void createKeyStoreFromAzureCosmosAccess() throws Exception {
         File keyStoreFile = File.createTempFile("azure-cosmos-emulator.keystore", "");
         keyStoreFile.mkdir();
         KeyStore keyStore = emulator.buildNewKeyStore();
